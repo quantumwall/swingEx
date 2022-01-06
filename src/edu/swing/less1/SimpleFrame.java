77ee52c133e4a67e30a52794407613e726a4bf48
ricotioloco <liberiamor@gmail.com>
2022-01-06 14:04:06 +0300

    second commit

---
+++ src/edu/swing/less1/SimpleFrame.java
@@ -0,0 +1,33 @@
+package edu.swing.less1;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class SimpleFrame {
+    public static void main(String[] args) { 
+        EventQueue.invokeLater(() ->
+            {
+                Toolkit toolkit = Toolkit.getDefaultToolkit();
+                Image icon = toolkit.getImage("/home/rico/itProjects/java/swing/src/edu/swing/less1/Mascot_konqi.png");
+                Dimension screenSize = toolkit.getScreenSize();
+                int screenWidth = screenSize.width;
+                int screenHeight = screenSize.height;
+
+                var frame = new JFrame("Title");
+                frame.setBounds(screenWidth/4,screenHeight/4,screenWidth / 2, screenHeight / 2);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setIconImage(icon);
+                frame.setVisible(true);
+            });
+    }
+}
+
+/*class SimpleFrameExt extends JFrame {
+    private static final int WIDTH = 300;
+    private static final int HEIGHT = 200;
+
+    public SimpleFrameExt() {
+        setSize(WIDTH, HEIGHT);
+    }
+}*/
+    
